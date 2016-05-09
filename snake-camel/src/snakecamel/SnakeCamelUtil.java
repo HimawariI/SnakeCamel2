2113eafa0e0b44798338b8700bba12e31e580855
HimawariI <g14911ih@gm.tsuda.ac.jp>
2016-05-09 13:49:40 +0900

    テストケース実装

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -7,7 +7,7 @@ public class SnakeCamelUtil {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < words.length; i++) {
-			if(words[i].length()==0)continue;
+			if(words[i].length()==0) continue;
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
